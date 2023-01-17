bd7f9cd32cbb127030e0e5ccf8ec1ad60336f268
JacquesIversen <jackiracki1@gmail.com>
2023-01-17 11:13:42 +0000

    Highscore final page added

--- assets/js/script.js
+++ assets/js/script.js
@@ -124,7 +124,7 @@ getNewQuestion = () => {
     const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
     const MAX_HIGH_SCORES = 10;
 
-    console.log("highScores")
+
     finalScore.innerText = mostRecentScore;
 
 
@@ -149,3 +149,14 @@ getNewQuestion = () => {
         localStorage.setItem("highScores", JSON.stringify(highScores));
         window.location.assign("/")
     };
+
+    // HighScore table HTML in js. 
+
+    const highScoresList = document.getElementById("highScoresList");
+    const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+    highScoresList.innerHTML = 
+    highScores.map(score => {
+        return '<li class="high-score>${score.name} - $score.score}</li></li>';
+    })
+    .join("");
