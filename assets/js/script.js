3bcdf52f01feb76810ca9f983788e80ae8cd2d5b
JacquesIversen <jackiracki1@gmail.com>
2023-01-18 20:05:31 +0000

    screw this commit bs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,92 +7,130 @@ let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuesions = [];
+let availableQuestions = [];
 
 let questions = [
   {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
+      question: "Who won the 2021 Formula 1 Drivers Championship",
+      choice1: "Charles Leclerc",
+      choice2: "Lewis Hamilton",
+      choice3: "Nicolas Latifi",
+      choice4: "Max Verstappen",
+      answer: 2
+  },
+  {
+      question: "Seriøst !? Det er da et lorte navn!",
+      choice1: "lille møgko",
+      choice2: "Store møgko",
+      choice3: "Grimme møgko",
+      choice4: "Store, Grimme lille møgko",
+      answer: 1
+  },
+  {
+      question: "Eller vent nu lige",
+      choice1: "lille møgko",
+      choice2: "Store møgko",
+      choice3: "Grimme møgko",
+      choice4: "Store, Grimme lille møgko",
+      answer: 1        
+  },
+  {
+      question: "Det faktisk OK!",
+      choice1: "lille møgko",                
+      choice2: "Store møgko",
+      choice3: "Grimme møgko",
+      choice4: "Store, Grimme lille møgko",
+      answer: 1
+  },
+  {
+    question: "Seriøst !? Det er da et lorte navn!",
+    choice1: "lille møgko",
+    choice2: "Store møgko",
+    choice3: "Grimme møgko",
+    choice4: "Store, Grimme lille møgko",
     answer: 1
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3
-  },
-  {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4
-  }
+},
+{
+    question: "Eller vent nu lige",
+    choice1: "lille møgko",
+    choice2: "Store møgko",
+    choice3: "Grimme møgko",
+    choice4: "Store, Grimme lille møgko",
+    answer: 1        
+},
+{
+    question: "Det faktisk OK!",
+    choice1: "lille møgko",                
+    choice2: "Store møgko",
+    choice3: "Grimme møgko",
+    choice4: "Store, Grimme lille møgko",
+    answer: 1
+},
+
 ];
 
-//CONSTANTS
+// Point system
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
-  questionCounter = 0;
-  score = 0;
-  availableQuesions = [...questions];
-  getNewQuestion();
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
 };
 
 getNewQuestion = () => {
-  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    //go to the end page
-    return window.location.assign("/end.html");
+
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+      // END PAGE
+      return window.location.assign("/highscore.html");
   }
   questionCounter++;
   progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-  //Update the progress bar
+
+  // Progress bar 
+  
   progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-  currentQuestion = availableQuesions[questionIndex];
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
 
-  choices.forEach(choice => {
-    const number = choice.dataset["number"];
-    choice.innerText = currentQuestion["choice" + number];
+  choices.forEach (choice => {
+      const number = choice.dataset["number"];
+      choice.innerText = currentQuestion["choice" + number];
   });
 
-  availableQuesions.splice(questionIndex, 1);
+  availableQuestions.splice(questionIndex, 1);
+
   acceptingAnswers = true;
 };
+
 
 choices.forEach(choice => {
   choice.addEventListener("click", e => {
     if (!acceptingAnswers) return;
 
-    acceptingAnswers = false;
-    const selectedChoice = e.target;
-    const selectedAnswer = selectedChoice.dataset["number"];
+      acceptingAnswers = false;
+      const selectedChoice = e.target;
+      const selectedAnswer = selectedChoice.dataset["number"];
 
-    const classToApply =
-      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+      const classToApply =
+        selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
     if (classToApply === "correct") {
       incrementScore(CORRECT_BONUS);
     }
 
-    selectedChoice.parentElement.classList.add(classToApply);
 
+    selectedChoice.parentElement.classList.add(classToApply);
+            
     setTimeout(() => {
       selectedChoice.parentElement.classList.remove(classToApply);
       getNewQuestion();
     }, 1000);
-  });
+  }); 
 });
 
 incrementScore = num => {
