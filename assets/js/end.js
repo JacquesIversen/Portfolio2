201e7ab0d83bddfe26b4499ad87f44796c3808a2
JacquesIversen <jackiracki1@gmail.com>
2023-01-19 01:01:44 +0000

    screw highscore page. that is now deleted.... fts.

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,44 +1,29 @@
-// HIGHSCORE PAGE
-
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [] ;
-const MAX_HIGH_SCORE = 5;
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-console.log(highScores);
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
-
-
 username.addEventListener('keyup', () => {
-  saveScoreBtn.disabled = !username.value;
+    saveScoreBtn.disabled = !username.value;
 });
 
-
-
-saveHighScore = e => {
-  console.log("Clicked the save button!");
-  e.preventDefault();
+saveHighScore = (e) => {
+    e.preventDefault();
 
     const score = {
-        score: Math.floor(Math.random() * 100),
+        score: mostRecentScore,
         name: username.value,
     };
-highScores.push(score);
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(5);
 
-highScores.sort((a,b) => {
-    return b.score - a.score;
-})
-
-highScores.splice(5);
-
-localStorage.setItem("highScores", JSON.stringify(highScores));
-window.location.assign("/index.html");
-
-console.log(highScores);
-
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('/');
 };
